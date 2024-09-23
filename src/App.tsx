13099a2f8ac3b3d93985c848236939f6d08116c2
Vorleak Yek <vorleakyek@gmail.com>
2024-09-22 23:44:47 -0700

    Update profile picture and add user details

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 import coverImage from './img/cover.jpg';
+import profile from './img/avatar.jpg';
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
         <div className="header-container">
           <div className="background">
-            <img className="background-img" src={coverImage} alt="" />
+            <img className="background-img" src={coverImage} alt="background image" />
           </div>
+          <div className='profile'>
+            <img src={profile} alt="image profile" />
+          </div>
+        </div>
+        <div className="header-details">
+          <div className='detais-info'>
+            <h2 className="profile-name">Jack Smith</h2>
+            <div className="flex">
+              <div className="username div-with-dot-lg-screen">
+                <p>@kingjack</p>
+              </div>
+              <div className="title">
+                <p>Senior Product Designer</p>
+              </div>
+              <div className="flex-row">
+                <div className="workplace div-with-dot">
+                  <p>at Webflow </p>
+                </div>
+                <div className="gender">
+                  <p>He/Him</p>
+                </div>
+              </div>
+
+            </div>
+
+           <div>
+              <p className="mt-0">Vancouver, Canada</p>
+           </div>
+
+          </div>
+
+
         </div>
       </header>
     </div>
