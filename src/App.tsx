f7611fe8ced08c6fba0ad6b6e3380952ca5afcbd
Vorleak Yek <vorleakyek@gmail.com>
2024-09-23 20:38:11 -0700

    update page responsive for information details

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { FaWebflow } from "react-icons/fa6";
+import flag from "./img/canada-flag-icon.svg";
 
+import './App.css';
 import coverImage from './img/cover.jpg';
-import profile from './img/avatar.jpg';
+import profile from './img/pika.png';
 
 
 function App() {
@@ -20,27 +22,29 @@ function App() {
         </div>
         <div className="header-details">
           <div className='detais-info'>
-            <h2 className="profile-name">Jack Smith</h2>
+            <h2 className="profile-name">Leslie Waller</h2>
             <div className="flex">
               <div className="username div-with-dot-lg-screen">
-                <p>@kingjack</p>
+                <p>@lesliewaller</p>
               </div>
               <div className="title">
                 <p>Senior Product Designer</p>
               </div>
               <div className="flex-row">
                 <div className="workplace div-with-dot">
-                  <p>at Webflow </p>
+                  <p> <span className="gray">at</span> <FaWebflow className="work-icon"/> Webflow </p>
                 </div>
-                <div className="gender">
-                  <p>He/Him</p>
+                <div className="gender gray">
+                  <p>She/Her</p>
                 </div>
               </div>
 
             </div>
 
            <div>
-              <p className="mt-0">Vancouver, Canada</p>
+              <p className="mt-0 gray">
+                <img src={flag} alt="flag" className="flag"/>
+                Vancouver, Canada</p>
            </div>
 
           </div>
